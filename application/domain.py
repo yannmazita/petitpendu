0588115222bb94a3559f5bc2baa01113f6f6b5b8
Yann Mazita <dev.mazita@gmail.com>
2023-03-06 13:42:00 +0100

    first commit

---
+++ application/domain.py
@@ -0,0 +1,74 @@
+from pathlib import Path
+
+
+class Joueur:
+    """Informations d'un joueur du pendu."""
+    nombreJoueurs: int = 0
+
+    def __init__(self):
+        Joueur.nombreJoueurs += 1
+        self.__nom: str = ""
+        self.__points: int = 0
+        self.__parties: int = 0
+        self.__ratioVictoires: float = 1
+
+    @property
+    def nom(self):
+        """Nom d'un joueur."""
+        return self.__nom
+
+    @nom.setter
+    def nom(self, nomDuJoueur):
+        self.__nom = nomDuJoueur
+
+    @property
+    def points(self):
+        """Points d'un joueur."""
+        return self.__points
+
+    @points.setter
+    def points(self, pointsJoueur):
+        self.__points = pointsJoueur
+
+    @property
+    def parties(self):
+        """Nombre de parties du joueur."""
+        return self.__parties
+
+    @parties.setter
+    def parties(self, partiesJoueur):
+        self.__parties = partiesJoueur
+
+    @property
+    def ratioVictoires(self):
+        """Ratio de victoires du joueur."""
+        return self.__ratioVictoires
+
+    @ratioVictoires.setter
+    def ratioVictoires(self, ratio):
+        self.__ratioVictoires = ratio
+
+
+class Dictionnaire:
+    """Informations sur le dictionnaire de mots"""
+    def __init__(self):
+        self.__chemin: Path = Path("")
+        self.__mot: str = ""
+
+    @property
+    def chemin(self):
+        """Chemin d'accès du dictionnaire"""
+        return self.__chemin
+
+    @chemin.setter
+    def chemin(self, cheminAcces):
+        self.__chemin = cheminAcces
+
+    @property
+    def mot(self):
+        """Mot du dictionnaire à trouver"""
+        return self.__mot
+
+    @mot.setter
+    def mot(self, motDictionnaire):
+        self.__mot = motDictionnaire
---
+++ data/dao.py
@@ -0,0 +1,103 @@
+from pathlib import Path
+from random import uniform
+from unidecode import unidecode
+import json
+
+from src.les_exceptions.le_pendu.application.domain import Joueur
+from src.les_exceptions.le_pendu.application.helpers import getProjectRoot
+
+
+class Dictionnaire:
+    """Accès aux données du dictionnaire."""
+
+    @staticmethod
+    def recupererMotAleatoire(path: Path) -> str:
+        """
+        Récupération d'un mot aléatoire dans un dictionnaire.
+        Args:
+            path: Le chemin du dictionnaire.
+
+        Returns:
+            str: Le mot aléatoire.
+        """
+        numeroLigne: int = 0
+        ligneSelect: str = ""
+        with open(path) as fichier:
+            while True:
+                ligne = fichier.readline()
+                if not ligne:  # Si la ligne est vide on arrête
+                    break
+                numeroLigne += 1
+                if uniform(0, numeroLigne) < 1:
+                    # Lorsqu'on a vu N lignes, la probabilité qu'une ligne soit sélectionnée est 1/N
+                    ligneSelect = ligne
+
+        motUnidecode: str = unidecode(ligneSelect.strip(), "utf-8")     # Tout accent est supprimé.
+        return motUnidecode
+
+
+class Save:
+    """Accès aux données des sauvegardes."""
+    class JoueurSerialisation(json.JSONEncoder):
+        """Sérialisation de quelques informations sur le joueur."""
+        def default(self, obj):     # Redéfinition de json.JSONEncoder.default() pour les instances de Joueur.
+            if isinstance(obj, Joueur):
+                data = {
+                    "__Joueur__": True,
+                    "nom": f"{obj.nom}",
+                    "points": f"{obj.points}",
+                    "ratio": obj.ratioVictoires,
+                    "parties": obj.parties
+                }
+                return data
+            else:
+                return super().default(obj)
+
+    @staticmethod
+    def joueurDeserialisation(dic: dict) -> Joueur | dict:
+        """
+        Désérialisation de quelques informations sur le joueur.
+        Args:
+            dic: Données sérialisées du joueur dans un dictionnaire.
+
+        Returns:
+            Joueur | dict: une instance de Joueur ou le dictionnaire lui-même.
+        """
+        if "__Joueur__" in dic:
+            joueur: Joueur = Joueur()
+            joueur.nom = dic["nom"]
+            joueur.points = dic["points"]
+            joueur.ratioVictoires = dic["ratio"]
+            joueur.parties = dic["parties"]
+            return joueur
+        else:
+            return dic
+
+    @staticmethod
+    def sauvegarderJoueur(joueur: Joueur) -> None:
+        """
+        Sauvegarde sur le disque de quelques informations sur le joueur.
+        Args:
+            joueur: Le joueur à sauvegarder.
+
+        Returns:
+            None
+        """
+        with open(Path(getProjectRoot() / "les_exceptions" / "le_pendu" / "sauvegardes" / f"{joueur.nom}.json"), "w") \
+                as fichier:
+            json.dump(joueur, fichier, indent=4, cls=Save.JoueurSerialisation)
+
+    @staticmethod
+    def chargerJoueur(nomJoueur: str) -> Joueur:
+        """
+        Chargement d'une sauvegarde d'un joueur.
+        Args:
+            nomJoueur: Le nom du joueur.
+
+        Returns:
+            Joueur: Une instance de joueur avec les données chargées.
+        """
+        with open(Path(getProjectRoot() / "les_exceptions" / "le_pendu" / "sauvegardes" / f"{nomJoueur}.json"), "r") \
+                as fichier:
+            data = fichier.read()
+            return json.loads(data, object_hook=Save.joueurDeserialisation)
