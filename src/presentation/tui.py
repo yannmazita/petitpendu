2987c622eb711fb6fb8c6a700c648e7bb611bd17
Yann Mazita <dev.mazita@gmail.com>
2023-03-06 20:25:47 +0100

    I/O fixes

--- src/presentation/tui.py
+++ src/presentation/tui.py
@@ -19,24 +19,24 @@ class Tui:
         Returns:
             None
         """
-        print("Combien de joueurs ? : ")
-        nombreJoueurs: str = input().strip()
+        sys.stdout.write("Combien de joueurs ? : ")
+        nombreJoueurs: str = sys.stdin.readline().strip()
 
         while not (re.search("^([1-9]|0[1-9]|[1-9]([0-9])+)$", nombreJoueurs)):
-            print("Seuls les nombres entiers strictement positifs sont autorisés.\nCombien de joueurs ? : ")
-            nombreJoueurs: str = input()
+            sys.stdout.write("Seuls les nombres entiers strictement positifs sont autorisés.\nCombien de joueurs ? : ")
+            nombreJoueurs: str = sys.stdin.readline()
 
         noms: list[str] = []
 
         for i in range(0, int(nombreJoueurs)):
             while True:
-                print(f"Quel est le nom du joueur {i + 1} ? : ")
-                nom = input().strip()
+                sys.stdout.write(f"Quel est le nom du joueur {i + 1} ? : ")
+                nom = sys.stdin.readline().strip()
                 try:
                     nom.encode('ascii')
                     break
                 except UnicodeEncodeError:
-                    print(f"Seuls les caractères ASCII sont autorisés.\n")
+                    sys.stdout.write(f"Seuls les caractères ASCII sont autorisés.\n")
 
             noms.append(nom)
 
@@ -55,8 +55,8 @@ class Tui:
         points: int = self.__jeu.joueurs[numeroJoueur].points
         parties: int = self.__jeu.joueurs[numeroJoueur].parties
         ratio: float = self.__jeu.joueurs[numeroJoueur].ratioVictoires
-        print(f"{nom}, vous avez actuellement {points} points.\n")
-        print(f"Vous avez gagné {ratio} % des {parties} partie(s) jouée(s).\n")
+        sys.stdout.write(f"{nom}, vous avez actuellement {points} points.\n")
+        sys.stdout.write(f"Vous avez gagné {ratio} % des {parties} partie(s) jouée(s).\n")
 
     def afficherMotCourant(self) -> None:
         """
@@ -65,8 +65,8 @@ class Tui:
             None
         """
         mot: list[str] = self.__jeu.motCourant
-        print(f"{' '.join(mot)}\n")
-        #print(f"{self.__jeu.dico.mot}\n")
+        sys.stdout.write(f"{' '.join(mot)}\n")
+        #sys.stdout.write(f"{self.__jeu.dico.mot}\n")
 
     def demanderLettre(self, numeroJoueur: int) -> None:
         """
@@ -78,13 +78,13 @@ class Tui:
             None
         """
         while True:
-            print(f"Joueur {numeroJoueur}, donnez une lettre (reste {self.__jeu.essais} essais)\n")
-            lettre: str = input().strip()
+            sys.stdout.write(f"Joueur {numeroJoueur}, donnez une lettre (reste {self.__jeu.essais} essais)\n")
+            lettre: str = sys.stdin.readline().strip()
             try:
                 self.__jeu.saisieLettre(lettre)
                 break
             except TypeError as e:
-                print(f"{e}")
+                sys.stdout.write(f"{e}")
 
     def afficherStatutPartie(self, numeroJoueur: int) -> bool | None:
         """
@@ -97,12 +97,12 @@ class Tui:
         """
         statut: bool | None = self.__jeu.determinerStatutPartie()
         if statut:
-            print(f"Bravo {self.__jeu.joueurs[numeroJoueur].nom}, vous avez trouvé en {self.__jeu.essais} "
+            sys.stdout.write(f"Bravo {self.__jeu.joueurs[numeroJoueur].nom}, vous avez trouvé en {self.__jeu.essais} "
                              f"essais le mot : {self.__jeu.dico.mot}\n")
-            print(f"Vous avez gagné {self.__jeu.joueurs[numeroJoueur].points} points\n")
+            sys.stdout.write(f"Vous avez gagné {self.__jeu.joueurs[numeroJoueur].points} points\n")
             return True
         elif statut is False:
-            print(f"Perdu!! Vous n'avez pas trouvé en {self.__jeu.essais} essais\n")
+            sys.stdout.write(f"Perdu!! Vous n'avez pas trouvé en {self.__jeu.essais} essais\n")
             return False
         elif statut is None:
             return None
@@ -113,11 +113,11 @@ class Tui:
         Returns:
             bool: True s'il désire continuer, False sinon.
         """
-        print(f"Voulez-vous continuer [y/n] ? : ")
-        choix: str = input().strip()
+        sys.stdout.write(f"Voulez-vous continuer [y/n] ? : ")
+        choix: str = sys.stdin.readline().strip()
         while choix not in ['y', 'n']:
-            print(f"Seuls 'y' ou 'n' sont autorisés\n")
-            choix = input().strip()
+            sys.stdout.write(f"Seuls 'y' ou 'n' sont autorisés\n")
+            choix = sys.stdin.readline().strip()
 
         if choix == 'y':
             return True
